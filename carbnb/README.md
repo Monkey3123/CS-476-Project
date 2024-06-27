e61a6b3865973695d990167de5d34eade1f5bd0b
Monkey3123 <leongtc@outlook.com>
2024-06-27 14:05:45 -0600

    Connected signup page to backend and confirmed was able to create new entries in database

--- carbnb/README.md
+++ carbnb/README.md
@@ -1,5 +1,6 @@
 # Getting Started with Create React App
 
+Note: run server by going into server file and running: node --env-file=config.env server
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- carbnb/src/components/SignUpPage/SignUpPage.js
+++ carbnb/src/components/SignUpPage/SignUpPage.js
@@ -1,54 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import carBanner from "../../images/option2.jpg";
 import "./SignupPage.css";
+import { useParams, useNavigate } from "react-router-dom";
 
 const SignUpPage = () => {
-  const [formData, setFormData] = useState({
-    fname: '',
-    lname: '',
-    email: '',
-    password: '',
-    cpassword: ''
+  const [form, setForm] = useState({
+    first: "",
+    last: "",
+    email: "",
+    password: "",
+    cpassword: "",
   });
 
-  const [errors, setErrors] = useState({});
+  const [isNew, setIsNew] = useState(true);
+  const params = useParams();
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData, [name]: value
+  useEffect(() => {
+    async function fetchData() {
+      const id = params.id?.toString() || undefined;
+      if (!id) return;
+      setIsNew(false);
+      const response = await fetch(
+        `http://localhost:5050/record/${params.id.toString()}`
+      );
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        console.error(message);
+        return;
+      }
+      const record = await response.json();
+      if (!record) {
+        console.warn(`Record with id ${id} not found`);
+        navigate("/");
+        return;
+      }
+      setForm(record);
+    }
+    fetchData();
+    return;
+  }, [params.id, navigate]);
+
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
     });
-  };
+  }
 
-  const handleSubmit = (e) => {
+  // This function will handle the submission.
+  async function onSubmit(e) {
     e.preventDefault();
     const validationErrors = {};
 
-    if (!formData.fname.trim()) {
-      validationErrors.fname = "First name is required";
+    if (!form.first.trim()) {
+      validationErrors.first = "First name is required";
     }
-    if (!formData.lname.trim()) {
-      validationErrors.lname = "Last name is required";
+    if (!form.last.trim()) {
+      validationErrors.last = "Last name is required";
     }
-    if (!formData.email.trim()) {
+    if (!form.email.trim()) {
       validationErrors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+    } else if (!/\S+@\S+\.\S+/.test(form.email)) {
       validationErrors.email = "Email is not valid";
     }
-    if (!formData.password.trim()) {
+    if (!form.password.trim()) {
       validationErrors.password = "Password is required";
-    } else if (formData.password.length < 8) {
+    } else if (form.password.length < 8) {
       validationErrors.password = "Password should be at least 8 characters";
-    } else if (!/(?=.*[a-z])/.test(formData.password)) {
+    } else if (!/(?=.*[a-z])/.test(form.password)) {
       validationErrors.password = "Password must contain one lowercase letter";
-    } else if (!/(?=.*[A-Z])/.test(formData.password)) {
+    } else if (!/(?=.*[A-Z])/.test(form.password)) {
       validationErrors.password = "Password must contain one uppercase letter";
-    } else if (!/(?=.*[0-9])/.test(formData.password)) {
+    } else if (!/(?=.*[0-9])/.test(form.password)) {
       validationErrors.password = "Password must contain one number";
-    } else if (!/(?=.*[!@#$%^&*])/.test(formData.password)) {
+    } else if (!/(?=.*[!@#$%^&*])/.test(form.password)) {
       validationErrors.password = "Password must contain one special character";
     }
-    if (formData.cpassword !== formData.password) {
+    if (form.cpassword !== form.password) {
       validationErrors.cpassword = "Passwords do not match";
     }
 
@@ -57,82 +86,153 @@ const SignUpPage = () => {
     if (Object.keys(validationErrors).length === 0) {
       alert("Form Submitted successfully");
       // Handle form submission, e.g., send data to the server
+
+      const person = { ...form };
+      try {
+        let response;
+        if (isNew) {
+          // if we are adding a new record we will POST to /record.
+          response = await fetch("http://localhost:5050/record", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+          });
+        } else {
+          // if we are updating a record we will PATCH to /record/:id.
+          response = await fetch(`http://localhost:5050/record/${params.id}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+          });
+        }
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+      } catch (error) {
+        console.error("A problem occurred with your fetch operation: ", error);
+      } finally {
+        setForm({ first: "", last: "", email: "", password: "" });
+        navigate("/");
+      }
     }
-  };
+  }
+
+  const [errors, setErrors] = useState({});
 
   return (
-    <div className="banner-image" style={{
-      backgroundImage: `url(${carBanner})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      height: "100vh"
-    }}>
+    <div
+      className="banner-image"
+      style={{
+        backgroundImage: `url(${carBanner})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+      }}
+    >
       <div className="signup template d-flex justify-content-center align-items-center vh-100">
-        <form className="form1 p-4 bg-light border rounded" onSubmit={handleSubmit}>
+        <form className="form1 p-4 bg-light border rounded" onSubmit={onSubmit}>
           <h1>Sign Up to CaRnR</h1>
           <div className="mb-3">
-            <label htmlFor="exampleInputFirstName" className="form-label">First Name</label>
+            <label htmlFor="exampleInputFirstName" className="form-label">
+              First Name
+            </label>
             <input
               type="text"
+              name="first"
+              id="first"
               className="form-control"
-              id="exampleFirstName"
-              name="fname"
-              value={formData.fname}
-              onChange={handleChange}
+              placeholder="first"
+              value={form.first}
+              onChange={(e) => updateForm({ first: e.target.value })}
             />
-            {errors.fname && <span className="text-danger">{errors.fname}</span>}
+            {errors.first && (
+              <span className="text-danger">{errors.first}</span>
+            )}
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputLastName" className="form-label">Last Name</label>
+            <label htmlFor="exampleInputLastName" className="form-label">
+              Last Name
+            </label>
             <input
               type="text"
+              name="last"
+              id="last"
               className="form-control"
-              id="exampleLastName"
-              name="lname"
-              value={formData.lname}
-              onChange={handleChange}
+              placeholder="last"
+              value={form.last}
+              onChange={(e) => updateForm({ last: e.target.value })}
             />
-            {errors.lname && <span className="text-danger">{errors.lname}</span>}
+            {errors.last && <span className="text-danger">{errors.last}</span>}
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
             <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
+              type="text"
               name="email"
-              value={formData.email}
-              onChange={handleChange}
+              id="email"
+              className="form-control"
+              placeholder="email"
+              value={form.email}
+              onChange={(e) => updateForm({ email: e.target.value })}
             />
-            {errors.email && <span className="text-danger">{errors.email}</span>}
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else (example abc@xyz.com).</div>
+            {errors.email && (
+              <span className="text-danger">{errors.email}</span>
+            )}
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else (example
+              abc@xyz.com).
+            </div>
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
             <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
+              type="text"
               name="password"
-              value={formData.password}
-              onChange={handleChange}
+              id="password"
+              className="form-control"
+              placeholder="password"
+              value={form.password}
+              onChange={(e) => updateForm({ password: e.target.value })}
             />
-            {errors.password && <span className="text-danger">{errors.password}</span>}
-            <div id="passwordHelp" className="form-text">Should contain one uppercase, one lowercase, one special character, and one number.</div>
+            {errors.password && (
+              <span className="text-danger">{errors.password}</span>
+            )}
+            <div id="passwordHelp" className="form-text">
+              Should contain one uppercase, one lowercase, one special
+              character, and one number.
+            </div>
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputPassword2" className="form-label">Confirm Password</label>
+            <label htmlFor="exampleInputPassword2" className="form-label">
+              Confirm Password
+            </label>
             <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword2"
+              type="text"
               name="cpassword"
-              value={formData.cpassword}
-              onChange={handleChange}
+              id="cpassword"
+              className="form-control"
+              placeholder="cpassword"
+              value={form.cpassword}
+              onChange={(e) => updateForm({ cpassword: e.target.value })}
             />
-            {errors.cpassword && <span className="text-danger">{errors.cpassword}</span>}
+            {errors.cpassword && (
+              <span className="text-danger">{errors.cpassword}</span>
+            )}
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <input
+            type="submit"
+            value="Create Account"
+            className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3 cursor-pointer mt-4"
+          />
         </form>
       </div>
     </div>
