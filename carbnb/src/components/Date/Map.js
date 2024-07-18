3a81d99f09276e761f7ad6b74b886ceac32be196
Monkey3123 <leongtc@outlook.com>
2024-07-18 15:41:21 -0600

    Updated map with search bar with autofill

--- carbnb/src/components/Date/Map.js
+++ carbnb/src/components/Date/Map.js
@@ -1,15 +1,71 @@
 "use client";
-
-import React from "react";
-import { useState } from "react";
-import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
+import "./mapStyles.css";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  APIProvider,
+  ControlPosition,
+  MapControl,
+  AdvancedMarker,
+  Map,
+  useMap,
+  useMapsLibrary,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
 
 export default function Intro() {
   const [Lat, setLat] = useState("");
   const [Long, setLong] = useState("");
 
-  const [MLat, MsetLat] = useState("");
-  const [MLong, MsetLong] = useState("");
+  const [myLat, mysetLat] = useState("");
+  const [myLong, mysetLong] = useState("");
+
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [markerRef, marker] = useAdvancedMarkerRef();
+
+  const MapHandler = ({ place, marker }) => {
+    const map = useMap();
+
+    useEffect(() => {
+      if (!map || !place || !marker) return;
+
+      if (place.geometry?.viewport) {
+        map.fitBounds(place.geometry?.viewport);
+      }
+      marker.position = place.geometry?.location;
+      mysetLat(marker.position.lat);
+      mysetLong(marker.position.lng);
+      console.log("LAT:", myLat, "LONG:", myLong);
+    }, [map, place, marker]);
+    return null;
+  };
+
+  const PlaceAutocomplete = ({ onPlaceSelect }) => {
+    const [placeAutocomplete, setPlaceAutocomplete] = useState(null);
+    const inputRef = useRef(null);
+    const places = useMapsLibrary("places");
+
+    useEffect(() => {
+      if (!places || !inputRef.current) return;
+
+      const options = {
+        fields: ["geometry", "name", "formatted_address"],
+      };
+
+      setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
+    }, [places]);
+    useEffect(() => {
+      if (!placeAutocomplete) return;
+
+      placeAutocomplete.addListener("place_changed", () => {
+        onPlaceSelect(placeAutocomplete.getPlace());
+      });
+    }, [onPlaceSelect, placeAutocomplete]);
+    return (
+      <div className="autocomplete-container">
+        <input ref={inputRef} />
+      </div>
+    );
+  };
 
   React.useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -22,18 +78,18 @@ export default function Intro() {
     <APIProvider apiKey={process.env.REACT_APP_API_KEY}>
       <div style={{ height: "50vh", width: "50%" }}>
         <Map
-          onClick={(ev) => {
-            console.log("latitide = ", ev.detail.latLng.lat);
-            console.log("latitide = ", ev.detail.latLng.lng);
-            MsetLat(ev.detail.latLng.lat);
-            MsetLong(ev.detail.latLng.lng);
-          }}
           defaultZoom={14}
           defaultCenter={{ lat: Lat, lng: Long }}
           mapId={process.env.REACT_APP_MAP_KEY}
         >
-          {MLat && <AdvancedMarker position={{ lat: MLat, lng: MLong }} />}
+          <AdvancedMarker ref={markerRef} position={null} />
         </Map>
+        <MapControl position={ControlPosition.TOP}>
+          <div className="autocomplete-control">
+            <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
+          </div>
+        </MapControl>
+        <MapHandler place={selectedPlace} marker={marker} />
       </div>
     </APIProvider>
   );
